d56747c85197d1062fd408ce3b5efd3361210b5b
WANJIN <wanjin@megazone.com>
2021-07-08 16:20:09 +0900

    chore: make spot automation routes root

--- src/routes/config.ts
+++ src/routes/config.ts
@@ -1,55 +1,55 @@
-// Views
-import ErrorPage from '@/common/pages/ErrorPage.vue';
-import SignOut from '@/common/pages/SignOut.vue';
-
-// Routes
-import signInRoute from '@/routes/sign-in/sign-in-route';
-import dashboardRoute from '@/routes/dashboard/dashboard-route';
-import identityRoute from '@/routes/identity/identity-route';
-import inventoryRoute from '@/routes/inventory/inventory-route';
-import pluginRoute from '@/routes/plugin/plugin-route';
-import projectRoute from '@/routes/project/project-route';
-import managementRoute from '@/routes/management/management-route';
-import automationRoute from '@/routes/automation/automation-route';
-import monitoringRoute from '@/routes/monitoring/monitoring-route';
 import { RouterOptions } from 'vue-router';
 
+const SpotAutomationMainPage = () => import(/* webpackChunkName: "SpotAutomationMainPage" */ '@/views/automation/spot-automation/pages/SpotAutomationMainPage.vue');
+const SpotDashboardPage = () => import(/* webpackChunkName: "SpotDashboardPage" */ '@/views/automation/spot-automation/pages/SpotDashboardPage.vue');
+const SpotGroupListPage = () => import(/* webpackChunkName: "SpotGroupPage" */ '@/views/automation/spot-automation/pages/SpotGroupListPage.vue');
+const SpotGroupDetailPage = () => import(/* webpackChunkName: "SpotGroupDetailPage" */ '@/views/automation/spot-automation/pages/SpotGroupDetailPage.vue');
+const AddSpotGroupPage = () => import(/* webpackChunkName: "AddSpotGroupPage" */ '@/views/automation/spot-automation/pages/AddSpotGroupPage.vue');
+
+
+const SPOT_AUTOMATION_ROUTE = Object.freeze({
+    _NAME: 'spotAutomation',
+    DASHBOARD: { _NAME: 'spotDashboard' },
+    SPOT_GROUP: {
+        _NAME: 'spotGroup',
+        ADD: { _NAME: 'addSpotGroup' },
+        DETAIL: { _NAME: 'spotGroupDetail' },
+    },
+});
 
 export const routerOptions = {
     mode: 'history',
     linkActiveClass: 'open active',
     routes: [
         {
-            path: '/error-page',
-            name: 'error',
-            meta: { label: '', excludeAuth: true },
-            component: ErrorPage,
-        },
-        {
-            path: '/sign-out',
-            name: 'SignOut',
-            component: SignOut,
-            meta: { label: '', excludeAuth: true, isSignInPage: false },
-        },
-        signInRoute,
-        {
             path: '/',
-            name: 'root',
-            meta: { label: 'root' },
-            redirect: '/dashboard',
-            component: { template: '<router-view />' },
+            name: SPOT_AUTOMATION_ROUTE._NAME,
+            redirect: 'spot-dashboard',
+            component: SpotAutomationMainPage,
             children: [
-                dashboardRoute,
-                identityRoute,
-                inventoryRoute,
-                pluginRoute,
-                projectRoute,
-                managementRoute,
-                automationRoute,
-                monitoringRoute,
+                {
+                    path: 'spot-dashboard',
+                    name: SPOT_AUTOMATION_ROUTE.DASHBOARD._NAME,
+                    component: SpotDashboardPage,
+                },
+                {
+                    path: 'spot-group',
+                    name: SPOT_AUTOMATION_ROUTE.SPOT_GROUP._NAME,
+                    component: SpotGroupListPage,
+                },
             ],
         },
-        { path: '*', component: ErrorPage },
+        {
+            path: 'spot-group/add/:projectId?',
+            name: SPOT_AUTOMATION_ROUTE.SPOT_GROUP.ADD._NAME,
+            component: AddSpotGroupPage,
+        },
+        {
+            path: 'spot-group/:id',
+            name: SPOT_AUTOMATION_ROUTE.SPOT_GROUP.DETAIL._NAME,
+            props: true,
+            component: SpotGroupDetailPage,
+        },
     ],
     duplicateNavigationPolicy: 'reload',
 } as RouterOptions;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,45 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import { SpaceConnector } from '@spaceone/console-core-lib/space-connector';
 import { routerOptions } from '@/routes/config';
-import { GTag } from '@/lib/gtag';
-import config from '@/lib/config';
 
 
 Vue.use(VueRouter);
 
 
 const router = new VueRouter(routerOptions);
-
-router.beforeEach(async (to, from, next) => {
-    if (to.meta && to.meta.excludeAuth) {
-        if (to.meta.isSignInPage) {
-            if (SpaceConnector.isTokenAlive) {
-                try {
-                    next({ path: to.meta.query.nextPath });
-                } catch (e) {
-                    next('/');
-                }
-            }
-        }
-        next();
-    } else if (SpaceConnector.isTokenAlive) {
-        const isAdmin = router.app.$store.getters['user/isAdmin'];
-        if (to.meta && to.meta.isDomainOwnerOnly && !isAdmin) {
-            next({ name: 'error' });
-        }
-        next();
-    } else if (!to.meta?.excludeAuth && !SpaceConnector.isTokenAlive) {
-        await router.push({ name: 'SignOut', query: { nextPath: to.fullPath, error: to.query.error } });
-        next();
-    } else {
-        next();
-    }
-});
-
-router.afterEach((to, from) => {
-    if (config.get('GTAG_ID') !== 'DISABLED') GTag.setPageView(to);
-});
 
 router.onError((error) => {
     if (/loading chunk \d* failed./i.test(error.message)) {
